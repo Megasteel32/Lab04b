e03cc6f88a0b4148e66c87cef66c7e05aae73f6c
Megasteel32 <megasteel32@gmail.com>
2020-09-14 20:58:33 -0500

    initial

---
+++ program2.py
@@ -0,0 +1,29 @@
+# By submitting this assignment, I agree to the following:
+#  "Aggies do not lie, cheat, or steal, or tolerate those who do"
+#  "I have not given or received any unauthorized aid on this assignment"
+#
+# Name:        Luca Maddaleni, 330001030
+# Section:     273
+# Assignment:  Reynold's Number
+# Date:        9/14/2020
+
+# Defining variables
+velocity = 0
+viscosity = 0
+linear_dimension = 0
+reynolds_number = 0
+
+# Takes inputs from user
+velocity = int(input("What is the velocity of the fluid? "))
+viscosity = int(input("What is the viscosity of the fluid? "))
+linear_dimension = int(input("What is the linear dimension of the fluid? "))
+reynolds_number = (velocity * linear_dimension) / viscosity
+
+# Defines which kind of flow
+
+if reynolds_number > 4000:
+    print("The Reynold's Number of the fluid is",reynolds_number,"and above 4000, meaning the fluid is turbulent. ")
+elif reynolds_number < 2100:
+    print("The Reynold's Number of the fluid is",reynolds_number,"and below 2100, meaning the fluid is Laminar. ")
+else:
+    print("The Reynold's Number of the fluid is",reynolds_number,"and between 2100 and 4000, meaning the fluid is transient. ")
