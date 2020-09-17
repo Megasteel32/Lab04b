966bed4a39e12a929ba24abfe9924fdc668d12e4
Megasteel32 <megasteel32@gmail.com>
2020-09-17 02:19:53 -0500

    penis

--- program4.py
+++ program4.py
@@ -23,21 +23,27 @@ user_stress = 0
 user_strain = 0
 
 # Asking for user input
-user_strain = int(input("What is the strain? Enter a value between 0 and 0.26 "))
+user_strain = float(input("What is the strain? Enter a value between 0 and 0.26 "))
 while user_strain > 0.26 or user_strain < 0:
-    user_strain = int(input("What is the strain? Enter a value between 0 and 0.26 "))
+    user_strain = float(input("What is the strain? Enter a value between 0 and 0.26 "))
 youngs_modulus = (a_b[1] - start_point[1]) / (a_b[0 - start_point[0]])
 
+# Calculating slopes of sections
 start_to_a_b = youngs_modulus
 a_b_to_c = (c[1] - a_b[1]) / (c[0] - a_b[0])
 c_to_d = (d[1] - c[1]) / (d[0] - c[0])
-d_to_e = (e[1] - d[1]) / (e[0] - e[0])
+d_to_e = (e[1] - d[1]) / (e[0] - d[0])
 
+# Calculating user_stress
 if user_strain >= 0 and user_strain <= 0.01:
     user_stress = start_to_a_b * user_strain
 elif user_strain >= 0.01 and user_strain <= 0.06:
-    user_stress = a_b_to_c * user_strain
+    user_stress = a_b_to_c * user_strain + a_b[1]
 elif user_strain >= 0.06 and user_strain <= 0.17:
-    user_stress = c_to_d * user_strain
+    user_stress = c_to_d * user_strain + c[1]
 else:
-    user_stress = d_to_e * user_strain
+    user_stress = d_to_e * user_strain + d[1]
+
+# Printing results
+print("The Youngs Modulus is", youngs_modulus, end=".\n")
+print("The stress of the strain value you entered is", user_stress, "ksi.")
